268ffcf7a1d20b15427c3999e115be6552e31b18
elbertbm <david_w0123@yahoo.com.tw>
2015-06-21 00:50:02 +0800

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,17 @@
+library(sqldf)
+##read dataset,label,variable names 
+train<-read.table("./UCI HAR Dataset/train/X_train.txt")
+trainlabel<-read.table("./UCI HAR Dataset/train/Y_train.txt")
+features<-read.table("./UCI HAR Dataset/features.txt")
+test<-read.table("./UCI HAR Dataset/test/X_test.txt")
+testlabel<-read.table("./UCI HAR Dataset/test/Y_test.txt")
+##merge train and test dataset with variable names
+total<-rbind(test,train)
+colnames(total)<-features[,2]
+##select column with mean and std
+partname<-sqldf("select V2 from features where (V2 like '%mean()%' or V2 like '%std()%') ")
+partname1<-partname[,1]
+part<-subset(total,select=partname1)
+##create table with mean of each variable selected
+partmean<-sapply(part,mean)
+write.table(partmean,"./partmean.txt",row.name=FALSE)
